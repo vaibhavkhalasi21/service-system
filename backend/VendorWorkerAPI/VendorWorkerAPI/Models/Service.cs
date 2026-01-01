625f60b146a328146eedf142b94d4b9f56e88a26
vaibhavkhalasi21 <vaibhavkhalasi3@gmail.com>
2026-01-01 15:22:36 +0530

    Delete backend/VendorWorkerAPI directory

--- backend/VendorWorkerAPI/VendorWorkerAPI/Models/Service.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VendorWorkerAPI.Models
-{
-    public class Service
-    {
-        public int Id { get; set; }
-        public string ServiceName { get; set; }
-        public decimal Price { get; set; }
-    }
-}
