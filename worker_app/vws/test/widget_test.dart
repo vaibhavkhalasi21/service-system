6a06ce6250f82a3db8ba2a56c62f31a4ec84e1ae
Vaibhav <vaibhavkhalasi3@gmail.com>
2026-02-13 21:51:36 +0530

    ip updated

--- worker_app/vws/test/widget_test.dart
+++ worker_app/vws/test/widget_test.dart
@@ -1,14 +1,14 @@
-// This is a basic Flutter widget test.
+// This is a basic Flutter widgets test.
 //
-// To perform an interaction with a widget in your test, use the WidgetTester
+// To perform an interaction with a widgets in your test, use the WidgetTester
 // utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// gestures. You can also use WidgetTester to find child widgets in the widgets
+// tree, read text, and verify that the values of widgets properties are correct.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:vws/main.dart';
+import 'package:vws/worker_home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
